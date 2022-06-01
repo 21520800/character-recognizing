2d609c59633d588e8dd44e5bd6a6bdc233245fe6
UIT.21520800 <21520800@gm.uit.edu.vn>
2022-06-01 17:31:22 +0700

    Create Activation.h

---
+++ Activation.h
@@ -0,0 +1,44 @@
+#ifndef ACTIVATION_H
+#define ACTIVATION_H
+
+#include "Matrix.h"
+
+class Activation {
+public:
+    Activation();
+    Activation(const Activation& orig);
+    virtual ~Activation();
+    
+    /****** activation functions *************************/
+
+    // Unit step
+    static float unitStep(float x);
+    static Matrix unitStep(Matrix m);
+
+    // Linear
+    static float linear(float m, float c, float x);
+    static Matrix linear(float m, float c, Matrix x);
+
+    // Sigmoid
+    static float sigmoid(float x);
+    static Matrix sigmoid(Matrix m);
+    
+    // TanSigmoid
+    static float tanSigmoid(float x);
+    static Matrix tanSigmoid(Matrix m);
+
+    // Piecewise Linear 
+
+    // Gaussian
+
+    /****** activation functions derivatives *************/
+
+    // Sigmoid
+    static float SigmoidDerivative(float x);
+    static Matrix SigmoidDerivative(Matrix m);
+    
+private:
+
+};
+
+#endif /* ACTIVATION_H */
