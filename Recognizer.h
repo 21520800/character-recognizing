2fa8419e01054107c6c81764c67d1f41414bdd02
UIT.21520800 <21520800@gm.uit.edu.vn>
2022-06-01 17:38:51 +0700

    Create Recognizer.h

---
+++ Recognizer.h
@@ -0,0 +1,36 @@
+#ifndef RECOGNIZER_H
+#define RECOGNIZER_H
+
+#include <Magick++.h>
+#include "Matrix.h"
+
+class Recognizer {
+public:
+    Recognizer();
+    Recognizer(const Recognizer& orig);
+    virtual ~Recognizer();
+    
+    char recognize(std::string path);
+    int loadWeights();
+    int getOutputMatrix();
+    char checkTheRange();
+    int train(int noOfIteration);
+    
+private:
+    std::string imgPath;
+    Magick::Image img;
+    Matrix inputMatrix, hiddenLayer1Matrix, hiddenLayer2Matrix, outputLayerMatrix;
+    int inputNodes, hiddenLayer1Nodes, hiddenLayer2Nodes, outputNodes;
+    Matrix weightMatrix1, weightMatrix2, weightMatrix3;
+    int trainSet;
+    int distinctChars;
+    std::string *trainedChars;
+    float *rangeData;
+    char *rangeChars;
+    float charValue;
+    float **weightMatrix1List,**weightMatrix2List,**weightMatrix3List;
+    
+    
+};
+
+#endif /* RECOGNIZER_H */
